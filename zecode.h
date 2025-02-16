85d9a9cf713a246cfcb1a82d829dbb770a7df6ea
alglsf666 <161010614+alglsf666@users.noreply.github.com>
2025-02-16 13:09:43 +0800

    Update add algorithm
    int add int            ;
    string add string  ;
    fault handling      .
    
    Sample Code:
        var a = input<int>();
        var b = input<int>();
        var c = a + b;
        output(c);
        var x = input<string>();
        var y = input<string>();
        var z = x + y;
        output(z);
    Input:
    321
    321
    321
    321
    
    Output:
    642
    321321

--- zecode.h
+++ zecode.h
@@ -10,7 +10,7 @@ void output(T t) {cout << t << endl;}
 template<>
 void output(bool flag) {cout << (flag? "true" : "false") << endl;}
 template<>
-void output(var v) 
+void output(var v)
 {
     if (v.type() == typeid(int)) {output(any_cast<int>(v));}
     else if (v.type() == typeid(double)) {output(any_cast<double>(v));}
@@ -25,3 +25,23 @@ template<typename T>
 T input() {T t;cin >> t;return t;}
 template<>
 bool input() {string s;cin >> s;return s == "true";}
+
+//operator +
+std::any operator+(const std::any& a, const std::any& b) {
+    // 检查两个 std::any 是否存储相同类型
+    if (a.type() != b.type()) {
+        throw std::invalid_argument("The types of the two std::any objects do not match.");
+    }
+
+    // 检查类型并执行相应操作
+    if (a.type() == typeid(int)) {
+        // 如果是 int 类型，进行加法运算
+        return std::any_cast<int>(a) + std::any_cast<int>(b);
+    } else if (a.type() == typeid(std::string)) {
+        // 如果是 std::string 类型，进行字符串拼接
+        return std::any_cast<std::string>(a) + std::any_cast<std::string>(b);
+    } else {
+        // 如果不支持的类型，抛出异常
+        throw std::invalid_argument("Unsupported type for operator +.");
+    }
+}
